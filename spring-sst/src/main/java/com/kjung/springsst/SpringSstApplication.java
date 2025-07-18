83e7c9e61e6342dd5e5cdb6d79bd113893d5bb8e
김정현 <wjdgus0117@ccmedia.co.kr>
2025-07-18 13:38:08 +0900

    [Feat]: Create Spring Project

---
+++ spring-sst/src/main/java/com/kjung/springsst/SpringSstApplication.java
@@ -0,0 +1,13 @@
+package com.kjung.springsst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSstApplication.class, args);
+    }
+
+}
---
+++ spring-sst/src/test/java/com/kjung/springsst/SpringSstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjung.springsst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
