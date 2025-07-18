b348a32242655ace409f9d5294f9b13351e4bd6e
김정현 <wjdgus0117@ccmedia.co.kr>
2025-07-18 18:00:18 +0900

    [Feat]: SpeechClient @Bean 설정

--- spring-stt/src/main/java/com/kjung/springsst/core/config/SttConfig.java
+++ spring-stt/src/main/java/com/kjung/springsst/core/config/SttConfig.java
@@ -3,31 +3,30 @@ package com.kjung.springsst.core.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechSettings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class SttConfig {
 
-    private final String credentialsPath;
 
-    public SttConfig(@Value("${google.cloud.speech.credentials.path}") String credentialsPath) {
-        this.credentialsPath = credentialsPath;
+    private final Resource gcsCredentials;
+
+    public SttConfig(@Value("classpath:stt.json") Resource gcsCredentials) {
+        this.gcsCredentials = gcsCredentials;
     }
 
     @Bean
     public SpeechClient speechClient() throws IOException {
         try {
-//            ClassPathResource resource = new ClassPathResource("google-credentials.json");
-            ClassPathResource resource = new ClassPathResource(credentialsPath);
-            InputStream credentialStream = resource.getInputStream();
 
-            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialStream);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(gcsCredentials.getInputStream());
 
             SpeechSettings speechSettings = SpeechSettings.newBuilder()
                     .setCredentialsProvider(() -> credentials)
@@ -37,7 +36,7 @@ public class SttConfig {
 
         } catch (Exception e) {
             // 개발 환경에서 credentials가 없을 경우 기본 인증 사용
-            System.out.println("Google credentials 파일을 찾을 수 없습니다. 기본 인증을 사용합니다.");
+            log.error("Google credentials 파일을 찾을 수 없습니다. 기본 인증을 사용합니다.");
             return SpeechClient.create();
         }
     }
